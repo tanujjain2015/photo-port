af378d999ab8578f2f0b5a70910026a301c8ea05
tanjain <tanuj.jain@oracle.com>
2020-07-24 18:22:18 -0700

    contact form chnages

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,17 @@
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+  const mockContactSelected = jest.fn();
+  const mockSetContactSelected = jest.fn();
+
+  it('renders', () => {
+    render(<Nav
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
+    />);
+  })
